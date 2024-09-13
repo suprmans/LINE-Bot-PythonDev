5f0dfb4ba9fd7b0f8bbf6f24f4a6955fd33eabfe
Shalong Samretngan <shalong.s@outlook.com>
2024-09-13 11:44:14 +0700

    flex with data

--- template/app/response_message.py
+++ template/app/response_message.py
@@ -1,5 +1,7 @@
 from linebot.v3.messaging import TextMessage, Emoji
 
+from dalert.dalert_disaster import disaster_alert
+
 def reponse_message(event):
     # print(event)
     request_message = event.message.text
@@ -9,18 +11,23 @@ def reponse_message(event):
             {
                 "index": 0,
                 "productId": "5ac1bfd5040ab15980c9b435",
-                "emojiId": "024"
+                "emojiId": "009"
             },
             {
                 "index": 18,
-                "productId": "5ac1bfd5040ab15980c9b435",
-                "emojiId": "062"
+                "productId": "5ac21c46040ab15980c9b442",
+                "emojiId": "006"
             },
         ]
         emojis = [Emoji(**emoji) for emoji in emoji_data]
         message = "$ สวัสดี from Dev $"
         response = TextMessage(text=message, emojis=emojis)
         return response
+    
+
+    if request_message.startswith("เตือนภัย"):
+        return disaster_alert(event)
+    
     
     else: 
         return TextMessage(text="ไม่พบข้อความอัตโนมัติ กรุณาลองใหม่อีกครั้ง")
